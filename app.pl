3d435bbf0dfe2490b7d88d84f7976ef622954ce3
Chris Forbes <chrisf@ijw.co.nz>
2012-09-30 14:57:08 +1300

    store gamedata properly

--- app.pl
+++ app.pl
@@ -6,12 +6,21 @@ use MojoX::Redis;
 
 my $redis = MojoX::Redis->new(server => '127.0.0.1:6379');
 
+my $ttl = 300;		# server-side ttl is 5 minutes. client is expected
+					# to ping again at about 60% of ttl.
+
 get '/ping' => method () {
 	my $ip = $self->tx->remote_address;
 	my $port = $self->param(port => 0);
 	my $new = $self->param(new => 0);
+	my $yaml = $self->param(yaml => '');
 
 	$redis->incr('games') if $new;
+
+	my $key = "$ip:$port";
+
+	$redis->set($key => $yaml);
+	$redis->expire($key => TTL());
 
 	$self->render(text => 'It works!');
 };
