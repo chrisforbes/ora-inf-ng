f89fa4fcdf1cb5084597f517c08b6021046cd584
Chris Forbes <chrisf@ijw.co.nz>
2012-09-30 14:03:55 +1300

    initial commit

---
+++ app.pl
@@ -0,0 +1,19 @@
+#!/usr/bin/env perl
+
+use Mojolicious::Lite;
+use Method::Signatures::Simple;
+use MojoX::Redis;
+
+my $redis = MojoX::Redis->new(server => '127.0.0.1:6379');
+
+get '/ping' => method () {
+	my $ip = $self->tx->remote_address;
+	my $port = $self->param(port => 0);
+	my $new = $self->param(new => 0);
+
+	$redis->incr('games') if $new;
+
+	$self->render(text => 'It works!');
+};
+
+app->start;
